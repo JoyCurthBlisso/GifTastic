d3bfab03a9c226b7c080fba031e68518667bcbe5
Joy Curth <joy.curth@comcast.net>
2018-01-28 18:57:53 -0500

    updated - removed comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,6 @@
 $(document).ready(function()
 {
-	// Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. 
-	// Save it to a variable called topics.
 
-	//Check if it's one of the 32
 	var nflTeams = ["Patriots", "Titans", "Eagles", "Broncos", "Saints", "Redskins", "Dolphins", "Giants", "Raiders", "Cardinals"];
 
 	function renderButtons() {
@@ -33,8 +30,11 @@ $(document).ready(function()
 			
 			$("#nflImages").html("");
 			$.each(response.data, function(index, item){
-				var img = $('<div>Rating: ' + item.rating + '</div><br/><img src="' + item.images.original.url + '" />');
+				var img = $('<div>Rating: ' + item.rating + '</div><br/><img src="' + item.images.original_still.url + '"data-animate="' + item.images.original.url + '"data-state="still" class="gif"/>');
+				console.log(img);
+				
 				$("#nflImages").append(img);
+
 			});
 	  	});
 	}
@@ -46,9 +46,9 @@ $(document).ready(function()
 	    var teamsLower = nflTeams.map(function(item, index){
 	    	return item.toLowerCase();
 	    });
-	    // if (nflTeam === a value in the array)
+
 	   	if (teamsLower.indexOf(nflTeam.toLowerCase()) > -1){
-	   	    //Show error already has that team
+	 
 			alert("Enter another NFL team!");
 		} else {
 			nflTeams.push(nflTeam);
@@ -68,53 +68,16 @@ $(document).ready(function()
 
 	renderButtons();
 });
-// make it so you can click to move and click to stop
+	  $(".gif").on("click", function() {
 
-// q
+      var state = $(this).attr("data-state");
+     
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
 
-// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
-// xhr.done(function(data) { console.log("success got data", data); });
-
-//pass input value and return 10 images
-
-//switch the protocol in the query URL from http to https
-// or the app may not work properly when deployed to Github Pages.
-
-//});
-
-
-// Your app should take the topics in this array and create buttons in your HTML.
-
-// Try using a loop that appends a button for each string in the array.
-
-// When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-// When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-
-// Under every gif, display its rating (PG, G, so on).
-
-// This data is provided by the GIPHY API.
-// Only once you get images displaying with button presses should you move on to the next step.
-
-
-// Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
-// Deploy your assignment to Github Pages.
-// Rejoice! You just made something really cool.
-
-
-
-
-
-// Minimum Requirements
-
-// Attempt to complete homework assignment as described in instructions. If unable to complete certain portions, please pseudocode these portions to describe what remains to be completed.
-
-
-
-
-// Create a README.md
-
-// Add a README.md to your repository describing the project. Here are some resources for creating your README.md. Here are some resources to help you along the way:
-
-
-// About READMEs
-// Mastering Markdown
